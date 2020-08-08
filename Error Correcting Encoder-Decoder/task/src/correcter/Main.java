38ca7767b5f62c62d647fcecfc30661ee084668e
Paulo Filipe <prnfilipe@gmail.com>
2020-08-08 15:56:47 +0100

    Stage 4 commit

--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -5,42 +5,44 @@ import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Main {
-    private static final Random RND = new Random();
+    private static final Random R = new Random();
     private static final int[] MASKS = {1, 2, 4, 8, 16, 32, 64, 128};
     
     public static void main(String[] args) {
         File sendFile = new File("send.txt");
         File receivedFile = new File("received.txt");
-        char[] fileChars = getCharsFromFile(sendFile);
-        byte[] fileBytes = bitwiseErrors(fileChars);
-        writeToFile(fileBytes, receivedFile);
+        byte[] fileBytes = getFileBytes(sendFile);
+        writeToFile(bitwiseErrors(fileBytes), receivedFile);
     }
     
-    // BITWISE ARITHMETICS Error Generator Method
-    private static byte[] bitwiseErrors(char[] chars) {
-        byte[] bytes = toByteArray(chars);
+    //Generates errors with bitwise arithmetics
+    private static byte[] bitwiseErrors(byte[] bytes) {
         IntStream.range(0, bytes.length)
                 .forEach(i -> {
-                    int mask = ~MASKS[RND.nextInt(MASKS.length)];
-                    bytes[i] ^= mask;
+                    int mask = ~MASKS[R.nextInt(MASKS.length)];
+                    bytes[i] ^= mask & 0xFF;
                     bytes[i] = (byte) ~bytes[i];
                 });
         return bytes;
     }
     
-    // STRING MANIPULATION Error Generator Method
-    private static byte[] stringManipErrors(char[] chars) {
-        byte[] bytes = new byte[chars.length];
-        for (int i = 0; i < chars.length; ++i) {
-            StringBuilder sb = new StringBuilder(Integer.toBinaryString(chars[i]));
-            while (sb.length() < 8) {
-                sb.insert(0, '0');
-            }
-            int n = RND.nextInt(8);
+    //Generates errors with string manipulation
+    private static byte[] stringManipErrors(byte[] bytes) {
+        for (int i = 0; i < bytes.length; ++i) {
+            StringBuilder sb = new StringBuilder(byteToBinaryString(bytes[i]));
+            int n = R.nextInt(8);
             sb.setCharAt(n, sb.charAt(n) == '0' ? '1' : '0');
             bytes[i] = (byte) Integer.parseInt(sb.toString(), 2);
         }
         return bytes;
+    }
+    
+    private static String byteToBinaryString(byte b) {
+        StringBuilder sb = new StringBuilder(Integer.toBinaryString(b));
+        while (sb.length() < 8) {
+            sb.insert(0, '0');
+        }
+        return sb.toString();
     }
     
     private static void writeToFile(byte[] bytes, File file) {
@@ -51,7 +53,7 @@ public class Main {
         }
     }
     
-    private static char[] getCharsFromFile(File file) {
+    private static byte[] getFileBytes(File file) {
         StringBuilder fileData = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             char[] buff = new char[64];
@@ -59,12 +61,12 @@ public class Main {
             while ((numRead = reader.read(buff)) != -1) {
                 String readData = String.valueOf(buff, 0, numRead);
                 fileData.append(readData);
-                buff = new char[64];
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return fileData.toString().toCharArray();
+        char[] dataChars = fileData.toString().toCharArray();
+        return toByteArray(dataChars);
     }
     
     private static byte[] toByteArray(char[] chars) {
