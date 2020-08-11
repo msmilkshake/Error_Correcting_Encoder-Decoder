7aab0efc0b4b404ae2eea371ab4f58d8fcd7a71e
Paulo Filipe <prnfilipe@gmail.com>
2020-08-11 15:34:55 +0100

    Stage 5 initial commit

---
+++ Error Correcting Encoder-Decoder/task/src/correcter/logic/HammingEncoder.java
@@ -0,0 +1,17 @@
+package correcter.logic;
+
+public class HammingEncoder {
+    
+    public static byte[] bitwiseEncoder(byte[] bytes) {
+        byte[] encodedBytes = new byte[bytes.length * 2];
+        int arrayPointer = 0;
+        byte buildByte = 0;
+        short buildCount = 0;
+        
+        for (byte b : bytes) {
+            continue;
+        }
+        
+        return encodedBytes;
+    }
+}
--- Error Correcting Encoder-Decoder/task/src/correcter/ui/TextUserInterface.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/ui/TextUserInterface.java
@@ -3,6 +3,7 @@ package correcter.ui;
 import correcter.logic.Decoder;
 import correcter.logic.Encoder;
 import correcter.logic.ErrorSimulator;
+import correcter.tool.Timer;
 import correcter.tool.Tool;
 
 import java.io.File;
@@ -15,12 +16,15 @@ public class TextUserInterface {
     private File RECEIVED_FILE;
     private File DECODED_FILE;
     
+    private Timer timer;
+    
     public TextUserInterface(Scanner scn) {
         this.SCN = scn;
-        SEND_FILE = new File("send.txt");
+        SEND_FILE = new File("shakespeare.txt");
         ENCODED_FILE = new File("encoded.txt");
         RECEIVED_FILE = new File("received.txt");
         DECODED_FILE = new File("decoded.txt");
+        timer = new Timer();
     }
     
     public void start() {
@@ -48,23 +52,50 @@ public class TextUserInterface {
     }
     
     private void encode() {
+        timer.start();
         byte[] sendBytes = Tool.readFileCharsAsBytes(SEND_FILE);
+        timer.stop();
+        System.out.println("File chars loaded in " + timer.get() + "ms");
+        timer.start();
         byte[] encodedBytes = Encoder.stringManipEncoder(sendBytes);
+        timer.stop();
+        System.out.println("String encoding took " + timer.get() + "ms");
+        timer.start();
         encodedBytes = Encoder.bitwiseEncoder(sendBytes);
+        timer.stop();
+        System.out.println("Bitwise encoding took " + timer.get() + "ms");
         Tool.writeToFile(encodedBytes, ENCODED_FILE);
     }
     
     private void send() {
+        timer.start();
         byte[] encodedBytes = Tool.readFileBytes(ENCODED_FILE);
+        timer.stop();
+        System.out.println("File bytes loaded in " + timer.get() + "ms");
+        timer.start();
         byte[] receivedBytes = ErrorSimulator.stringManipErrors(encodedBytes);
+        timer.stop();
+        System.out.println("String errors took " + timer.get() + "ms");
+        timer.start();
         receivedBytes = ErrorSimulator.bitwiseErrors(encodedBytes);
+        timer.stop();
+        System.out.println("Bitwise errors took " + timer.get() + "ms");
         Tool.writeToFile(receivedBytes, RECEIVED_FILE);
     }
     
     private void decode() {
+        timer.start();
         byte[] receivedBytes = Tool.readFileBytes(RECEIVED_FILE);
+        timer.stop();
+        System.out.println("File bytes loaded in " + timer.get() + "ms");
+        timer.start();
         byte[] decodedBytes = Decoder.stringManipDecode(receivedBytes);
+        timer.stop();
+        System.out.println("String decoding took " + timer.get() + "ms");
+        timer.start();
         decodedBytes = Decoder.bitwiseDecode(receivedBytes);
         Tool.writeToFile(decodedBytes, DECODED_FILE);
+        timer.stop();
+        System.out.println("Bitwise decoding took " + timer.get() + "ms");
     }
 }
