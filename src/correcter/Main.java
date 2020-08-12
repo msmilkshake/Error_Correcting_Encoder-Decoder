a7fbc990cd8df66953393cf33537cbec5c60b6a0
Paulo Filipe <prnfilipe@gmail.com>
2020-08-12 13:27:19 +0100

    Stage 4 cleanup

---
+++ src/correcter/Main.java
@@ -0,0 +1,12 @@
+package correcter;
+
+import correcter.ui.TextUserInterface;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        new TextUserInterface(new Scanner(System.in)).start();
+    }
+}
